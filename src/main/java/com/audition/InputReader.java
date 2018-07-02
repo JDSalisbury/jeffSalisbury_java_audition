fb044672db50b7be9a5efa4db5cafa3a4a527748
JDSalisbury <Salisbury.jeffery@gmail.com>
2018-07-02 10:13:05 -0400

    input.txt is being read and the info is being added to a list

--- src/main/java/com/audition/InputReader.java
+++ src/main/java/com/audition/InputReader.java
@@ -1,5 +1,41 @@
 package com.audition;
 
-public class InputReader {
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.List;
 
+import com.audition.Objects.PlayerOne;
+
+public class InputReader {
+	public static List<PlayerOne> readPlayerOneFromInputFile(String fileName) {
+		List<PlayerOne> playerOnesGames = new ArrayList<>();
+
+		Path pathToFile = Paths.get(fileName);
+		try (BufferedReader reader = Files.newBufferedReader(pathToFile)) {
+			String line = reader.readLine();
+
+			while (line != null) {
+				String[] setUp = line.split(" ");
+				PlayerOne black = setUpPlayerOne(setUp);
+				playerOnesGames.add(black);
+
+				line = reader.readLine();
+
+			}
+
+		} catch (IOException e) {
+			e.printStackTrace();
+
+		}
+		return playerOnesGames;
+	}
+
+	private static PlayerOne setUpPlayerOne(String[] meta) {
+		String name = meta[0];
+		return new PlayerOne(name);
+	}
 }
