c71b2fe89e16371c2c810fef8c4f8ba41b357b67
JDSalisbury <Salisbury.jeffery@gmail.com>
2018-07-02 11:50:08 -0400

    im going to make hand extend the rules of poker

---
+++ src/test/java/com/audition/HandTest.java
@@ -0,0 +1,26 @@
+package com.audition;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.assertThat;
+
+import org.junit.Test;
+
+import com.audition.Objects.Card;
+import com.audition.Objects.Hand;
+
+public class HandTest {
+
+	private Card card1 = new Card('9', 'H');
+	private Card card2 = new Card('T', 'H');
+	private Card card3 = new Card('J', 'H');
+	private Card card4 = new Card('Q', 'H');
+	private Card card5 = new Card('K', 'H');
+	private Hand testHand = new Hand(card1, card2, card3, card4, card5);
+
+	@Test
+	public void shouldReturnHighCardInHand() {
+		Card theHighCardInHand = testHand.getHighCard();
+		assertThat(theHighCardInHand, is(card5));
+	}
+
+}
