231bbb55b3dca069d06a570f5dc8de5d03a907b6
JDSalisbury <Salisbury.jeffery@gmail.com>
2018-07-02 10:50:58 -0400

    So I have to add the hand, to the player

--- src/test/java/com/audition/InputReaderTest.java
+++ src/test/java/com/audition/InputReaderTest.java
@@ -13,15 +13,22 @@ import com.audition.Objects.PlayerOne;
 
 public class InputReaderTest {
 
+	private List<PlayerOne> gamesBlack = InputReader.readPlayerOneFromInputFile("input.txt");
+	private List<Hand> handsBlack = InputReader.readPlayerOnesCards("input.txt");
+
 	@Test
 	public void shouldReadInputAndGrabAllOfPlayerOnesGames() {
-		List<PlayerOne> gamesBlack = InputReader.readPlayerOneFromInputFile("input.txt");
 		assertThat(gamesBlack.size(), is(not(0)));
 	}
 
 	@Test
 	public void shouldReadInputAndGrabAllOfPlayerOnesHands() {
-		List<Hand> handsBlack = InputReader.readPlayerOnesCards("input.txt");
 		assertThat(handsBlack.size(), is(not(0)));
+	}
+
+	@Test
+	public void shouldAddHandsToPlayer() {
+		InputReader.addHandToPlayerOne(gamesBlack, handsBlack, "input.txt");
+
 	}
 }
